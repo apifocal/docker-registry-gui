7517dd0a6652876c5884e402f562b70828e44320
Andrei Stefan <slicer@sliceratwork.com>
2017-04-04 18:50:12 +0300

    use tgz extension for dist archive

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,8 @@ module.exports = function(grunt) {
     compress: {
       main: {
         options: {
-          archive: 'archive.<%= pkg.version %>.zip'
+          mode: "tgz",
+          archive: '<%= pkg.name %>.<%= pkg.version %>.tgz'
         },
         files: [
           {
